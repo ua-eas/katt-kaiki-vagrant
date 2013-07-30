031776e486f6ed959b7c4552f91c7f862e35eae2
Josh Shaloo <shaloo@email.arizona.edu>
2013-07-30 08:39:37 -0700

    some more tweaks to the bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,3 @@
 # ======================== Install some prerequisites ======================= #
 
 echo "running bootstrap.sh to provision the box"
-
-apt-get -y install openconnect xvfb ffmpeg
-
-
-
